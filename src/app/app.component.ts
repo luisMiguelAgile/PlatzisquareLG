4aadc7ff0d0bb1369bb05e3552087e300f16b814
unknown <luismiguel.agile@gmail.com>
2018-09-03 21:50:11 -0500

    adicion de mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzisquareLG';
+  lugares: any = [
+    { active:true, nombre: 'Donas la gordita' },
+    { active:true, nombre: 'Veterinaria huellas' },
+    { active:false, nombre: 'Floristeria la x' },
+    { active:true, nombre: 'PaloMiulato' }
+  ];
+
+  
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  constructor() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAnrmfEFY5RF47AzR7SR9TLMO9R7fVSmcA'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
